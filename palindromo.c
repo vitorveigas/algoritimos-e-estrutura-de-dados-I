1a64145d78990f0c1be3146ca679db21fc30ffea
Vitor Veiga <vitorveiga0621@gmail.com>
2024-04-08 11:55:54 -0300

    palindromo.c

--- palindromo.c
+++ palindromo.c
@@ -40,3 +40,47 @@ int main(void) {
 	return 0;
 }
 
+// palindromo resolução 2
+
+/*
+ ============================================================================
+ Name        : pjr_palindromo.c
+ Author      :  
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isPalindrome(char str[], int left, int right){
+    return(left >= right) ? 1 : (str[left] == str[right]) && isPalindrome(str, left + 1, right - 1);
+ }
+    int isPalindrome2(char str[], int left, int right){
+        if (left >= right)
+        return 1;
+        
+        return isPalindrome2(str, left + 1, right - 1);
+    }
+int main(void){
+    char palavra[100];
+    printf("Digite uma palavra:  ");
+    fflush(stdout);
+    scanf("%s", palavra);
+    int tamanho = strlen(palavra);
+    printf("%d", tamanho);
+    if (isPalindrome(palavra, 0, tamanho - 1)) { //verificar se é palindromo
+    printf(" É um palindromo\n");
+}else{
+    printf(" Não é palindromo\n");
+}
+
+return 0;
+
+}
+
+
+
