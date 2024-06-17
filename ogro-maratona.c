8d116fabfdd40f8e7af406a9199269006c21168c
Vitor Veiga <vitorveiga0621@gmail.com>
2024-06-17 11:44:21 -0300

    Update ogro-maratona.c

--- ogro-maratona.c
+++ ogro-maratona.c
@@ -1,38 +1,27 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
-
 
 int main() {
     int N;
-    printf("Digite o numero de dedos do ogro: \n");
     scanf("%d", &N);
 
-    if (N < 0 || N > 10) {
-        printf("Número fora do intervalo permitido.\n");
-        return 1;
-    }
-
-    int esquerda = (N <= 5) ? N : 5;
-    int direita = (N <= 5) ? 0 : N - 5;
-
-    
-    for (int i = 0; i < esquerda; i++) {
-        printf("I");
-    }
-    for (int i = esquerda; i < 5; i++) {
-        printf(" ");
+    if (N <= 5) {
+        for (int i = 0; i < N; ++i) {
+            printf("I");
+        }
+    } else {
+        printf("IIIII");
     }
     printf("\n");
 
-   
-    for (int i = 0; i < direita; i++) {
-        printf("I");
+    if (N <= 5) {
+        printf("*\n");
+    } else {
+        for (int i = 0; i < N - 5; ++i) {
+            printf("I");
+        }
+        printf("\n");
     }
-    for (int i = direita; i < 5; i++) {
-        printf(" ");
-    }
-    printf("\n");
 
     return 0;
-    
 }
